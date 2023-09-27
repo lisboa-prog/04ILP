de7ea995a446db04d9c4f435a55c0bfb6c358016
Lisboa-Prog <lisboa.prog@gmail.com>
2023-09-27 19:12:50 -0400

    Exercicio1 Finalizado

--- Aula02/Exercico1.py
+++ Aula02/Exercico1.py
@@ -1,2 +1,8 @@
 # Elaborar um algoritmo que solicita dois números ao usuário e exibe a soma destes números.
 
+num1 = int(input("Digite o 1º valor: "))
+num2 = int(input("Digite o 2º valor: "))
+
+soma = num1+num2
+
+print("\n soma = ",soma)
