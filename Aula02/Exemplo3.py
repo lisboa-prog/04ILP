01d7e7691fbb16a0b9586c8610821b633a231676
Lisboa-Prog <lisboa.prog@gmail.com>
2023-09-27 19:05:44 -0400

    Cadastro de Cliente finalizado

---
+++ Aula02/Exemplo3.py
@@ -0,0 +1,11 @@
+nome_cliete = input("Nome Do Cliente: ")
+telefone_cliete = input("Telefone: ")
+endereco_cliete = input("Endereço: ")
+idade_cliete = int(input("Idade: "))
+saldo_atual_cliete = float(input("Saldo:"))
+status_cliete = input("Estatus do Cliente(True/False): ")
+
+print('\n\n Dados do Cliente  \n\n','nome: ', nome_cliete, '\n telefone: ', telefone_cliete, 
+      '\n endereço: ', endereco_cliete, '\n idade: ', idade_cliete, '\n saldo: ', saldo_atual_cliete, 
+      '\n status_cliete: ',status_cliete 
+      )
