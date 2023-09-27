1825c05196b28845bc2518cf6a95059ee7f7ee5b
Lisboa-Prog <lisboa.prog@gmail.com>
2023-09-27 18:48:56 -0400

    Inserido comentario sobre a função do codigo

--- Aula02/Exemplo2.py
+++ Aula02/Exemplo2.py
@@ -1,3 +1,5 @@
+# Exemplo de aplicação 1: Elaborar um algoritmo que peça ao usuário um número e informe o quadrado deste número.
+
 num = int(input("Qual o número deseja elevar ao quadrado?: "))
 quad=num*num
 print('\n o quadrado de ',num,' vale ',quad)
