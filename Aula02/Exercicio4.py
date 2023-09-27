8e390d69e94cb6254633545f110c26d6a8179c7b
Lisboa-Prog <lisboa.prog@gmail.com>
2023-09-27 19:32:13 -0400

    Exercicio4 finalizado

--- Aula02/Exercicio4.py
+++ Aula02/Exercicio4.py
@@ -1,2 +1,13 @@
 # Elaborar um algoritmo que solicita o nome de um produto, seu valor e
 # quantidade, informando o valor de compra calculado.
+
+nome_cliente = input("Digite o seu nome: ")
+valor_produto = float(input("Digite o valor do produto: "))
+quantidade_de_produto = int(input("Digite a quantidade de Produto: "))
+
+valor_total_da_compra = valor_produto*quantidade_de_produto
+
+print("\n\nDados de compra")
+print('\nNome: ',nome_cliente)
+print(f'\nValor total: ,{valor_total_da_compra:.2fff}')
+
