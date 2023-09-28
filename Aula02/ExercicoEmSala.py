51d361f1b94f840682b41f1532c2f36afe057321
Lisboa-Prog <lisboa.prog@gmail.com>
2023-09-27 20:07:31 -0400

    Codigo finalizado

--- Aula02/ExercicoEmSala.py
+++ Aula02/ExercicoEmSala.py
@@ -3,8 +3,10 @@ Nota2 = float(input("Digite a 2ª nota: "))
 Nota3 = float(input("Digite a 3ª nota: "))
 Nota4 = float(input("Digite a 4ª nota: "))
 
-media= (Nota1+Nota2+Nota3+Nota4)/4
+media = (Nota1+Nota2+Nota3+Nota4)/4
+
 print(f"Media = {media:.2f}")
+
 if media >= 7:
     print("Aprovado")
 else:
