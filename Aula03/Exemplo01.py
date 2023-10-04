144b285cc1bac122f62e520216429037ed11b6cf
Lisboa-Prog <lisboa.prog@gmail.com>
2023-10-04 18:15:30 -0400

    Exemplo01 - media finalizado

---
+++ Aula03/Exemplo01.py
@@ -0,0 +1,9 @@
+nota1 = float(input("nota1: "))
+nota2 = float(input("nota2: "))
+
+media = (nota1+nota2)/2
+
+if media >= 7:
+    print("Você está aprovado")
+else:
+    print("Você está reprovado")
